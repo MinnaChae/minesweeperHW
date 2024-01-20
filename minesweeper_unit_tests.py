9dac0a073c669ce9f91edb9ec59dd100c7ce068c
Sarah St. Albin <130087873+sstalbin@users.noreply.github.com>
2024-01-20 15:10:45 -0800

    Finish unit tests.

--- minesweeper_unit_tests.py
+++ minesweeper_unit_tests.py
@@ -4,6 +4,7 @@ Assignment: Minesweeper
 Class: TCSS 504
 """
 import unittest
+import os
 import filecmp
 from unittest.mock import patch
 from minesweeper_team_solution import Minesweeper
@@ -23,12 +24,10 @@ class MinesweeperTests(unittest.TestCase):
 
         minesweeper_instance.run_minesweeper_solution()
 
-        # Assert that the attributes are set correctly
+        # Assert that the attributes are set correctly, object is properly instantiated
         self.assertEqual(minesweeper_instance.input_file, input_file)
         self.assertEqual(minesweeper_instance.output_file, output_file)
 
-        # Assert that the output is what is expected given the input
-        self.assertTrue(filecmp.cmp(output_file, "expected_output.txt", shallow=False))
 
     def test_run_minesweeper_solution(self):
         """
@@ -39,22 +38,11 @@ class MinesweeperTests(unittest.TestCase):
         input_file = "test_input.txt"
         output_file = "test_output.txt"
 
-        mock_field = ['...**', '.....', '.....', '*....', '.*.*.']
-
         minesweeper_instance = Minesweeper(input_file, output_file)
+        minesweeper_instance.run_minesweeper_solution()
 
-        with patch.object(minesweeper_instance, 'read_field') as mock_read_field:
-            mock_read_field.return_value = (5, mock_field)
-
-        with patch.object(minesweeper_instance, 'process_the_field') as mock_process_the_field:
-            pass
-
-        with patch.object(minesweeper_instance, 'write_to_output') as mock_write_to_output:
-            pass
-
-            minesweeper_instance.run_minesweeper_solution()
-
-            mock_read_field.assert_called()
+        # Assert that the output is what is expected given the input (the method actually solves the problem)
+        self.assertTrue(filecmp.cmp(output_file, "expected_output.txt", shallow=False))
 
     def test_read_field(self):
         """
@@ -116,33 +104,66 @@ class MinesweeperTests(unittest.TestCase):
         # Assert that the method is counting mines correctly
         self.assertEqual(expected_mine_count, test_mine_count, "The method did not correctly count adjacent mines")
 
-    # def test_write_to_output(self):
-    #     """
-    #     Tests basic functionality of write_to_output.
-    #     :return:
-    #     """
-    #     input_file = "test_input.txt"
-    #     output_file = "test_output.txt"
-    #
-    #     mock_processed_field = [['001**', '00122', '11000', '*2211', '2*2*1']]
-    #
-    #     with patch("builtins.open", mock_open()) as mock_file:
-    #         minesweeper_instance = Minesweeper(input_file, output_file)
-    #         minesweeper_instance.write_to_output(mock_processed_field)
-    #
-    #         calls = [mock.call(output_file, "w")]
-    #
-    #         # Assert that open was called with correct args
-    #         mock_file.assert_has_calls(calls, any_order=False)
-    #         # Assert that write was called
-    #         mock_file().write.assert_called()
-    #
-    #         # Get the content written to the file
-    #         written_content = mock_file().write.call_args[0][0]
-    #
-    #         # Assert that the content matches the expected format
-    #         expected_content = "Field #1:\n001**\n00122\n11000\n*2211\n2*2*1\n"
-    #         self.assertEqual(written_content, expected_content)
+    def test_write_to_output(self):
+        """
+        Tests basic functionality of write_to_output: it writes to the output file with the correct formatting.
+        :return:
+        """
+        input_file = "test_input_multiple_fields.txt"
+        output_file = "test_output_multiple_fields.txt"
+        expected_output = "expected_output_multiple_fields.txt"
+
+        minesweeper_instance = Minesweeper(input_file, output_file)
+
+        mock_field = [
+            ['*100', '2210', '1*10', '1110'],
+            ['**100', '33200', '1*100'],
+            ['*'],
+            ['0']
+        ]
+
+        minesweeper_instance.write_to_output(mock_field)
+
+        with open(output_file, "r") as file:
+            output_content = file.read()
+
+        with open(expected_output, "r") as expected_file:
+            expected_content = expected_file.read()
+
+        self.assertEqual(output_content, expected_content)
+
+    def test_given_minefield_proper_formatting(self):
+        """
+        Tests that a minefield is formatted properly.
+        """
+        input_file = "test_input.txt"
+        output_file = "test_output.txt"
+        expected_output = "expected_output.txt"
+
+        minesweeper_instance = Minesweeper(input_file, output_file)
+
+        minesweeper_instance.run_minesweeper_solution()
+
+        with open(output_file, "r") as file:
+            formatted_minefield = file.read()
+            first_line = file.readline()
+
+        with open(expected_output, "r") as expected_file:
+            expected_minefield = expected_file.read()
+            expected_first_line = expected_file.readline()
+
+        # Assert that first line of numbers is replaced with "Field #"
+        self.assertEqual(first_line, expected_first_line)
+
+        # Assert that all the safe rooms have integers in them and not "."
+        for actual_row, expected_row in zip(formatted_minefield, expected_minefield):
+            for actual_cell, expected_cell in zip(actual_row.strip(), expected_row.strip()):
+                if expected_cell == ".":
+                    # Check that the corresponding actual cell is an integer
+                    self.assertTrue(actual_cell.isdigit(), msg=f"Expected integer in cell, got {actual_cell}")
+
+        # Assert that overall formatting is correct
+        self.assertEqual(formatted_minefield, expected_minefield)
 
 
 
